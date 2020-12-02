1b13323776b954a80a7de2b0a7372e6a310ea587
Lucy <frenchline707@gmail.com>
2020-12-02 20:43:25 +0900

    IoC 컨테이너 3: @Autowire | @Qualifier
    같은 타입의 빈이 여러 개인 경우, 주입을 못 해준다.
    둘 중 어떤 걸 원하는 지 스프링은 알 수가 없다.
    
    해결: @Qualifier("주입 받을 Bean 이름")
    using @Qualifier to identify the bean that should be consumed
    
    - @Qualifier는 Bean의 id를 명시해 구분하는 방식이다
    - 기본적으로 @Component 계열 애노테이션을 쓰면 Bean의 id는 소문자(small case)로 시작하는 클래스 이름과 동일하게 만들어진다
    - 해당 Bean을 사용하는 쪽에서 @Qualifer("Bean의 id")를 명시해 주입 받는다
    
    cf. @Qualifier 보다는 @Primary 가 Type Safe 하므로 그 방법을 추천한다.

--- src/main/java/me/frenchline/spring52/BookService.java
+++ src/main/java/me/frenchline/spring52/BookService.java
@@ -1,12 +1,13 @@
 package me.frenchline.spring52;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
-    @Autowired
+    @Autowired @Qualifier("frenchBookRepository") //주입받을 Bean의 이름을 명시
     BookRepository bookRepository;
 
     /* BookService에 어떤 BookRepository 타입의 Bean이 주입이 되었는지 출력하는 메서드 */
--- src/main/java/me/frenchline/spring52/FrenchBookRepository.java
+++ src/main/java/me/frenchline/spring52/FrenchBookRepository.java
@@ -3,9 +3,10 @@ package me.frenchline.spring52;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
-/* marking one of the beans as @Primary */
-// 여러가지 BookRepository 타입이 있지만 그중 @Primary가 붙은 타입을 주로 사용할 것이다
-// BookRepository 타입의 여러가지 빈을 주입해야 되는 경우에 @Primary가 붙은 타입의 빈을 반드시 주입하도록 마킹
-@Repository @Primary
+/* using @Qualifier to identify the bean that should be consumed */
+// @Qualifier는 Bean의 id
+// 기본적으로 @Component 계열 애노테이션을 쓰면 Bean의 id는 소문자(small case)로 시작하는 클래스 이름과 동일하게 만들어진다
+// 해당 Bean을 사용하는 쪽에서 @Qualifer("Bean의 id")를 명시하거나, 필드명을 동일하게 맞춘다 
+@Repository
 public class FrenchBookRepository implements BookRepository {
 }
