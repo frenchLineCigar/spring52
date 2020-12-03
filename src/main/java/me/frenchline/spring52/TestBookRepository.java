345318904fbafe80a91c1985565d70456925d394
Lucy <frenchline707@gmail.com>
2020-12-03 16:05:31 +0900

    IoC 컨테이너 6: Environment 1. 프로파일
    프로파일 정의하기
    ● 클래스에 정의: 컴포넌트 스캔으로 등록되는 빈에도 Profile을 정의할 수 있다.
        ○ @Component @Profile(“test”)
    
    따라서 메타 애노테이션 @Component를 사용한 컴포지트 애노테이션에도 마찬가지로 정의할 수 있다.
    ○ @Repository @Profile("test")
    ○ @Service @Profile("test")
    ○ @Controller @Profile("test")
    ○ @Configuration @Profile("test")

--- src/main/java/me/frenchline/spring52/TestBookRepository.java
+++ src/main/java/me/frenchline/spring52/TestBookRepository.java
@@ -1,4 +1,9 @@
 package me.frenchline.spring52;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("test") //"test" profile에서 사용할 Repository다 (-Dspring.profiles.active="test")
 public class TestBookRepository implements BookRepository {
 }
