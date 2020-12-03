6ba8490ba060f2be88d09cc568dd140c0218756e
Lucy <frenchline707@gmail.com>
2020-12-03 19:04:43 +0900

    IoC 컨테이너 7: MessageSource
    국제화 (i18n) 기능을 제공하는 인터페이스.
    
    * MessageSource 관련된 기능은 ApplicationContext를 주입 받아 사용해도 된다.(ApplicationContext extends MessageSource 이므로)
    * 그런데 좋은 코딩 방법은 아니다.
    내가 쓰려는 기능을 정의한 인터페이스의 타입으로 쓰는 것이 더 적절하다.
    어떤 의도로 쓰는지 더 구체적이므로 좀 더 가독성이 높고 알기 쉽다.

--- src/main/java/me/frenchline/spring52/AppRunner.java
+++ src/main/java/me/frenchline/spring52/AppRunner.java
@@ -3,7 +3,7 @@ package me.frenchline.spring52;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.MessageSource;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
@@ -12,7 +12,13 @@ import java.util.Locale;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    MessageSource messageSource;
+    ApplicationContext messageSource;
+    /**
+     * MessageSource 관련된 기능은 ApplicationContext를 주입 받아 사용해도 된다. (ApplicationContext extends MessageSource 이므로)
+     * 그런데 좋은 코딩 방법은 아니다.
+     * 내가 쓰려는 기능을 정의한 인터페이스의 타입으로 쓰는 것이 더 적절하다.
+     * 어떤 의도로 쓰는지 더 구체적이므로 좀 더 가독성이 높고 알기 쉽다.
+     */
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
