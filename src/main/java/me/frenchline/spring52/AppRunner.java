8d080cc38ccab2767f386f9c72f808516eebedeb
Lucy <frenchline707@gmail.com>
2020-12-03 18:01:22 +0900

    IoC 컨테이너 7: MessageSource
    국제화 (i18n) 기능을 제공하는 인터페이스.
    
    ApplicationContext extends MessageSource
        ● getMessage(String code, Object[] args, String, default, Locale, loc)
        ● ...
    
    스프링 부트를 사용한다면 별다른 설정 필요없이 messages.properties 사용할 수 있음
        ● messages.properties
        ● messages_ko_kr.properties
        ● ...

--- src/main/java/me/frenchline/spring52/AppRunner.java
+++ src/main/java/me/frenchline/spring52/AppRunner.java
@@ -1,29 +1,27 @@
 package me.frenchline.spring52;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationContext ctx;
-
-    @Autowired
-    BookRepository bookRepository;
-
-    @Value("${app.name}")
-    String appName;
+    MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Environment environment = ctx.getEnvironment();
-        System.out.println(environment.getProperty("app.name"));
-        System.out.println(appName);
+        System.out.println(messageSource.getClass()); //Spring Boot의 경우 'messages'라는 Resource Bundle을 읽어들이는 ResourceBundleMessageSource가 이미 Bean으로 자동 등록되어 있다
+
+        System.out.println(messageSource.getMessage("greeting", new String[]{"lucy"}, Locale.getDefault())); //getDefault: 운영체제의 기본값으로 읽어온다
+        System.out.println(messageSource.getMessage("greeting", new String[]{"lucy"}, Locale.KOREA));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"lucy"}, Locale.US));
+//        System.out.println(messageSource.getMessage("greeting", new String[]{"lucy"}, Locale.KOREAN));
+//        System.out.println(messageSource.getMessage("greeting", new String[]{"lucy"}, Locale.ENGLISH));
     }
 }
--- src/main/java/me/frenchline/spring52/BookRepository.java
+++
@@ -1,4 +0,0 @@
-package me.frenchline.spring52;
-
-public interface BookRepository {
-}
