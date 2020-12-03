f6f9be5684b4e3c22b64d130b902ba8c2b32d261
Lucy <frenchline707@gmail.com>
2020-12-03 16:53:48 +0900

    IoC 컨테이너 6: Environment 2. 프로퍼티
    프로퍼티에는 우선 순위가 있다.
    ● StandardServletEnvironment의 우선순위
        ○ ServletConfig 매개변수
        ○ ServletContext 매개변수
        ○ JNDI (java:comp/env/)
        ○ JVM 시스템 프로퍼티 (-Dkey=”value”)
        ○ JVM 시스템 환경 변수 (운영 체제 환경 변수)

--- src/main/java/me/frenchline/spring52/AppRunner.java
+++ src/main/java/me/frenchline/spring52/AppRunner.java
@@ -19,7 +19,8 @@ public class AppRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = ctx.getEnvironment(); //ApplicationContext extends EnvironmentCapable
-        System.out.println(environment.getProperty("app.name")); //VM Options: -Dapp.name=spring5
-        System.out.println(environment.getProperty("app.about")); //app.properties
+        //우선 순위 확인: VM Options vs. app.properties
+        //계층형(Hierarchy) 쿼리니까 둘 중 하나만 이기게 된다
+        System.out.println(environment.getProperty("app.name")); //VM Options Win!
     }
 }
