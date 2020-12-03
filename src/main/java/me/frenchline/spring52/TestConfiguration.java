dd1541e69739566ab65320568c5399ff9449db8c
Lucy <frenchline707@gmail.com>
2020-12-03 15:48:32 +0900

    IoC 컨테이너 6: Environment 1. 프로파일
    프로파일 정의하기
    ● 메소드에 정의
        ○ @Bean @Profile(“test”)

--- src/main/java/me/frenchline/spring52/TestConfiguration.java
+++ src/main/java/me/frenchline/spring52/TestConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test") //이 Bean 설정파일은 "test" profile로 애플리케이션을 실행할 때만 사용된다
 public class TestConfiguration {
 
     @Bean
+    @Profile("test") //이 Bean 설정파일은 "test" profile로 애플리케이션을 실행할 때만 사용된다
     public BookRepository bookRepository() {
         return new TestBookRepository();
     }
