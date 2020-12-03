4cfbe782cbbb2122b5bb74614381b87ee0508a23
Lucy <frenchline707@gmail.com>
2020-12-03 18:35:54 +0900

    IoC 컨테이너 7: MessageSource
    국제화 (i18n) 기능을 제공하는 인터페이스.
    
    릴로딩 기능이 있는 메시지 소스 사용하기
    ```
    @Bean
    public MessageSource messageSource() {
        var messageSource = new ReloadableResourceBundleMessageSource();
        messageSource.setBasename("classpath:/messages"); //원하는 경로로 재설정 가능
        messageSource.setDefaultEncoding("UTF-8");
        messageSource.setCacheSeconds(3);
        return messageSource;
    }
    ```

--- src/main/java/me/frenchline/spring52/Spring52Application.java
+++ src/main/java/me/frenchline/spring52/Spring52Application.java
@@ -2,7 +2,10 @@ package me.frenchline.spring52;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 @PropertySource("classpath:/app.properties") //해당 위치의 properties파일을 PropertySource에 놓겠다
@@ -10,5 +13,16 @@ public class Spring52Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring52Application.class, args);
+    }
+
+    /* 릴로딩 기능이 있는 메시지 소스 사용 */
+    // ReloadableResourceBundleMessageSource 등록
+    // bean 이름은 항상 messageSource가 되야 한다
+    @Bean
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/testdir/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
