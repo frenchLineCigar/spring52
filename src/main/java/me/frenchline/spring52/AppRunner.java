d36fb0458ac505bf32a53c92120af4c3eaf0e6e6
Lucy <frenchline707@gmail.com>
2020-12-04 23:12:49 +0900

    Resource 추상화
    : org.springframework.core.io.Resource
    
    리소스 읽어오기
    ● Resource의 타입은 locaion 문자열과 ApplicationContext의 타입(구현체 타입)에 따라 결정 된다.
        ○ ClassPathXmlApplicationContext -> ClassPathResource
        ○ FileSystemXmlApplicationContext -> FileSystemResource
        ○ WebApplicationContext -> ServletContextResource
    ● ApplicationContext의 타입에 상관없이 리소스 타입을 강제하려면 java.net.URL 접두어(+ classpath:)중 하나를 사용할 수 있다.
        ○ classpath:me/whiteship/config.xml -> ClassPathResource
        ○ file:///some/resource/path/config.xml -> FileSystemResource
    
    특징
    ● java.net.URL을 추상화 한 것.
    : org.springframework.core.io.Resource로 감싸서 실제 raw레벨에 있는 리소스 자체를 추상화 시킴
    ● 스프링 내부에서 많이 사용하는 인터페이스.
    Ex) ApplicationContext를 만들 때의 코드
    var ctx = new ClassPathXmlApplicationContext("applicationContext.xml"); //클래스패스 기준으로 문자열에 준 설정 파일을 찾는다
    이 xml 문자열도 바로 Resource 추상화로 내부적으로 변환이 된다. 마치 다음 코드처럼 내부적으로 작동된다.
    Resource resource = resourceLoader.getResource(classpath:applicatioContext.xml);
    그래서 실질적으로 스프링 내부에서는 다 Resource를 사용하고 있는 것이다. 이미 우리들은 이 Resource를 사용해왔던 것이다.
    또한 한가지 차이점을 비교해보자.
    var ctx = new FileSystemXmlApplicationContext("applicationContext.xml"); //파일 시스템경로 기준으로 문자열에 해당하는 리소스를 찾아서 빈 설정파일로 사용
    
    추상화 한 이유 (기존의 java.net.URL 클래스의 기능 부재)
    ● 클래스패스 기준으로 리소스 읽어오는 기능 부재: 접두어(prefix) classpath:를 지원하기 위해서
    ● ServletContext를 기준으로 상대 경로로 읽어오는 기능 부재
    ● 새로운 핸들러를 등록하여 특별한 URL 접미사(suffix)를 만들어 사용할 수는 있지만 구현이 복잡하고 편의성 메소드가 부족하다.
    
    인터페이스 둘러보기
    ● 상속 받은 인터페이스
    ● 주요 메소드
        ○ getInputStream()
        ○ exist() : 존재하는 리소스인지? (boolean)
        ○ isOpen() : 리소스가 열려있는지? (boolean)
        ○ getDescription(): 전체 경로 포함한 파일 이름 또는 실제 URL
    
    구현체 : 굉장히 많지만 주목해서 봐야할 구현체
    ● UrlResource: java.net.URL 참고, 기본으로 지원하는 프로토콜 http, https, ftp, file, jar.
    ● ClassPathResource: 지원하는 접두어(prefix) classpath:
    ● FileSystemResource
    ● ServletContextResource: 웹 애플리케이션 루트에서 상대 경로로 리소스 찾는다. (사실상 가장 많이 쓰게 됨)
    가장 많이 쓰는 이유? 읽어 들이는 리소스 타입이 ApplicationContext와 관련이 있기 때문이다.
    
    ● ...

--- src/main/java/me/frenchline/spring52/AppRunner.java
+++ src/main/java/me/frenchline/spring52/AppRunner.java
@@ -3,8 +3,8 @@ package me.frenchline.spring52;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
@@ -14,26 +14,17 @@ import java.nio.file.Path;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass());
 
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        Resource resource = resourceLoader.getResource("classpath:test.txt"); //prefix를 명시적으로 사용하자
+        System.out.println(resource.getClass());
 
         System.out.println(resource.exists());
         System.out.println(resource.getDescription()); //리소스 설명
         System.out.println(Files.readString(Path.of(resource.getURI()))); //URI로 Path를 만들고 그 패스에 해당하는 파일을 읽어서 컨텐츠를 출력
-        System.out.println();
-        System.out.println(resource.isFile()); //파일 인지
-        System.out.println(resource.isReadable()); //읽을 수 있는지
-        System.out.println(resource.isOpen()); //열려 있는지
-        System.out.println(resource.getFilename()); //파일 이름
-        System.out.println(resource.getURI());
-        System.out.println(resource.getURL());
-        System.out.println(resource.getFile());
-//        System.out.println(resource.contentLength());
-//        System.out.println(resource.lastModified());
-//        System.out.println(resource.readableChannel());
     }
 }
