f8fe2d8ddb0a6bbd174fbea384d0a077308bb0f3
Lucy <frenchline707@gmail.com>
2020-12-04 23:23:53 +0900

    Resource 추상화
    : org.springframework.core.io.Resource
    
    리소스 읽어오기
    ● Resource의 타입은 locaion 문자열과 ApplicationContext의 타입에 따라 결정 된다.
        ○ ClassPathXmlApplicationContext -> ClassPathResource
        ○ FileSystemXmlApplicationContext -> FileSystemResource
        ○ WebApplicationContext -> ServletContextResource
    ● ApplicationContext의 타입에 상관없이 리소스 타입을 강제하려면  locaion 문자열에 java.net.URL 접두어(+ classpath:)중 하나를 사용하면 강제할 수 있다.
        ○ classpath:me/whiteship/config.xml -> ClassPathResource
        ○ file:///some/resource/path/config.xml -> FileUrlResource

--- src/main/java/me/frenchline/spring52/AppRunner.java
+++ src/main/java/me/frenchline/spring52/AppRunner.java
@@ -3,8 +3,8 @@ package me.frenchline.spring52;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
@@ -14,15 +14,11 @@ import java.nio.file.Path;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationContext resourceLoader;
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(resourceLoader.getClass());
-
         Resource resource = resourceLoader.getResource("classpath:test.txt"); //prefix를 명시적으로 사용하자
-        System.out.println(resource.getClass());
-
         System.out.println(resource.exists());
         System.out.println(resource.getDescription()); //리소스 설명
         System.out.println(Files.readString(Path.of(resource.getURI()))); //URI로 Path를 만들고 그 패스에 해당하는 파일을 읽어서 컨텐츠를 출력
