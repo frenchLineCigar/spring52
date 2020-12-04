f018375df8c25db5c270cc91a49534c6757ed387
Lucy <frenchline707@gmail.com>
2020-12-04 14:27:05 +0900

    IoC 컨테이너 9: ResourceLoader
    리소스를 읽어오는 기능을 제공하는 인터페이스
    
    ApplicationContext extends ResourceLoader
    
    리소스 읽어오기
    ● 파일 시스템에서 읽어오기
    ● 클래스패스에서 읽어오기
    ● URL로 읽어오기
    ● 상대/절대 경로로 읽어오기
    
    Resource getResource(java.lang.String location)

--- src/main/java/me/frenchline/spring52/AppRunner.java
+++ src/main/java/me/frenchline/spring52/AppRunner.java
@@ -3,18 +3,37 @@ package me.frenchline.spring52;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher publishEvent;
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        //이벤트를 발생시킨다(publish)
-        publishEvent.publishEvent(new MyEvent(this, 100));
+
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription()); //리소스 설명
+        System.out.println(Files.readString(Path.of(resource.getURI()))); //URI로 Path를 만들고 그 패스에 해당하는 파일을 읽어서 컨텐츠를 출력
+        System.out.println();
+        System.out.println(resource.isFile()); //파일 인지
+        System.out.println(resource.isReadable()); //읽을 수 있는지
+        System.out.println(resource.isOpen()); //열려 있는지
+        System.out.println(resource.getFilename()); //파일 이름
+        System.out.println(resource.getURI());
+        System.out.println(resource.getURL());
+        System.out.println(resource.getFile());
+//        System.out.println(resource.contentLength());
+//        System.out.println(resource.lastModified());
+//        System.out.println(resource.readableChannel());
     }
 }
--- src/main/java/me/frenchline/spring52/Spring52Application.java
+++ src/main/java/me/frenchline/spring52/Spring52Application.java
@@ -3,27 +3,12 @@ package me.frenchline.spring52;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync //비동기적으로 실행하려면 @EnableAsync 를 사용
 public class Spring52Application implements ExitCodeGenerator {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring52Application.class, args);
-    }
-
-    /* 릴로딩 기능이 있는 메시지 소스 사용 */
-    @Bean
-    public MessageSource messageSource() {
-        var messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setCacheSeconds(3); //리소스 캐쉬 시간: 최대 3초까지만 이 리소스를 캐싱하고 다시 읽도록 설정
-        return messageSource;
     }
 
     @Override
