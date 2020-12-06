ba2df0c62ee0e3d894ce2096b781ef643006f592
Lucy <frenchline707@gmail.com>
2020-12-06 16:44:24 +0900

    스프링 AOP: @AOP | @Before("bean(simpleEventService)")
    @Before 사용
    
    ---
    공통
    
    애노테이션 기반의 스프링 @AOP
    
    1. 의존성 추가
    <dependency>
        <groupId>org.springframework.boot</groupId>
        <artifactId>spring-boot-starter-aop</artifactId>
    </dependency>
    
    2. 애스팩트 정의
    ● @Aspect
    ● 빈으로 등록해야 하니까 (컴포넌트 스캔을 사용한다면) @Component도 추가.
    
    3. 포인트컷 정의
    ● @Pointcut(표현식)
    ● 주요 표현식
        ○ execution
        ○ @annotation
        ○ bean
    ● 포인트컷 조합
        ○ &&, ||, !
    
    4. 어드바이스 정의
    ● @Before
    ● @AfterReturning
    ● @AfterThrowing
    ● @Around
    
    참고
    ● https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#aoppointcuts

--- src/main/java/me/frenchline/spring52/PerfAspect.java
+++ src/main/java/me/frenchline/spring52/PerfAspect.java
@@ -3,6 +3,7 @@ package me.frenchline.spring52;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,13 +11,19 @@ import org.springframework.stereotype.Component;
 public class PerfAspect {
 
     /* Advice 구현 */
-    //이 Advice를 어떻게 적용할 것인가?를 정의
+    /* 어떤 메소드 실행 전후로 */
     @Around("bean(simpleEventService)") //bean이 가지고 있는 모든 public 메서드에 적용된다
     public Object logPerf(ProceedingJoinPoint pjp) throws Throwable { //ProceedingJoinPoint: Advice가 적용되는 지점(Target의 해당 메서드)
         long begin = System.currentTimeMillis();
         Object retVal = pjp.proceed();
         System.out.println(System.currentTimeMillis() - begin);
         return retVal;
+    }
+
+    /* 어떤 메소드가 실행되기 이전 */
+    @Before("bean(simpleEventService)")
+    public void hello() {
+        System.out.println("hello");
     }
 
 }
