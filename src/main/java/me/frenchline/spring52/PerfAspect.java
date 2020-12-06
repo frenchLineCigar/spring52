1cfec348a0ffa8b3ee256c461fd004c553b1ca45
Lucy <frenchline707@gmail.com>
2020-12-06 16:38:31 +0900

    스프링 AOP: @AOP | @Around("@annotation(PerfLogging)")
    Pointcut 이름 (애노테이션 사용)
    
    ---
    공통
    
    애노테이션 기반의 스프링 @AOP
    
    1. 의존성 추가
    <dependency>
        <groupId>org.springframework.boot</groupId>
        <artifactId>spring-boot-starter-aop</artifactId>
    </dependency>
    
    2. 애스팩트 정의
    ● @Aspect
    ● 빈으로 등록해야 하니까 (컴포넌트 스캔을 사용한다면) @Component도 추가.
    
    3. 포인트컷 정의
    ● @Pointcut(표현식)
    ● 주요 표현식
        ○ execution
        ○ @annotation
        ○ bean
    ● 포인트컷 조합
        ○ &&, ||, !
    
    4. 어드바이스 정의
    ● @Before
    ● @AfterReturning
    ● @AfterThrowing
    ● @Around
    
    참고
    ● https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#aoppointcuts

--- src/main/java/me/frenchline/spring52/PerfAspect.java
+++ src/main/java/me/frenchline/spring52/PerfAspect.java
@@ -11,8 +11,7 @@ public class PerfAspect {
 
     /* Advice 구현 */
     //이 Advice를 어떻게 적용할 것인가?를 정의
-    @Around("execution(* me.frenchline..*.EventService.*(..))") //메서드를 감싸는(Around) 형태로 (Pointcut을 직접 정의)
-    //@Around("execution(* me.frenchline..*.*(..))") //해당 패키지 하위 모든 패키지의 모든 메서드
+    @Around("@annotation(PerfLogging)") //메서드를 감싸는(Around) 형태로 (Pointcut 이름)
     public Object logPerf(ProceedingJoinPoint pjp) throws Throwable { //ProceedingJoinPoint: Advice가 적용되는 지점(Target의 해당 메서드)
         long begin = System.currentTimeMillis();
         Object retVal = pjp.proceed();
