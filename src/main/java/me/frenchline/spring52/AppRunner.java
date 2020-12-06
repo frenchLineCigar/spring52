8da920b030ffe59be5faf9912bb2ac90d1701f08
Lucy <frenchline707@gmail.com>
2020-12-06 14:09:52 +0900

    SpEL (스프링 Expression Language)
    스프링 EL이란?
    ● 객체 그래프를 조회하고 조작하는 기능을 제공한다.
    ● Unified EL과 비슷하지만, 메소드 호출을 지원하며, 문자열 템플릿 기능도 제공한다.
    ● OGNL, MVEL, JBOss EL 등 자바에서 사용할 수 있는 여러 EL이 있지만, SpEL은 모든 스프링 프로젝트 전반에 걸쳐 사용할 EL로 만들었다.
    ● 스프링 3.0 부터 지원.
    
    SpEL 구성
    ● ExpressionParser parser = new SpelExpressionParser()
    ● StandardEvaluationContext context = new StandardEvaluationContext(bean)
    ● Expression expression = parser.parseExpression(“SpEL 표현식”)
    ● String value = expression.getvalue(context, String.class)
    
    문법
    ● #{“표현식"}
    ● ${“프로퍼티"}
    ● 표현식은 프로퍼티를 가질 수 있지만, 반대는 안 됨.
        ○ #{${my.data} + 1}
    ● 레퍼런스 참고
    
    실제로 어디서 쓰나?
    ● @Value 애노테이션
    ● @ConditionalOnExpression 애노테이션
    ● 스프링 시큐리티
        ○ 메소드 시큐리티, @PreAuthorize, @PostAuthorize, @PreFilter, @PostFilter
        ○ XML 인터셉터 URL 설정
        ○ ...
    ● 스프링 데이터
        ○ @Query 애노테이션
    ● Thymeleaf
    ● ...
    
    [참고]
    스프링 EL: https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#expressions
    Unified EL: https://docs.oracle.com/javaee/5/tutorial/doc/bnahq.html
    스프링 EL 레퍼런스: https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#expressions-language-ref
    스프링 시큐리티 SpEL: https://docs.spring.io/spring-security/site/docs/3.0.x/reference/el-access.html
    스프링 데이터 SpEL: https://spring.io/blog/2014/07/15/spel-support-in-spring-data-jpa-query-definitions
    Thymeleaf SpEL: https://blog.outsider.ne.kr/997

--- src/main/java/me/frenchline/spring52/AppRunner.java
+++ src/main/java/me/frenchline/spring52/AppRunner.java
@@ -1,21 +1,85 @@
 package me.frenchline.spring52;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.convert.ConversionService;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    ConversionService conversionService;
+    /* SpEL (Spring Expression Language) */
 
-    /* ConversionService를 Bean을 받아올 수 있는지 확인 */
+    //1) #{“표현식"}
+    @Value("#{1 + 1}") //표현식 평가(evaluation) 후
+    int value; //결과 값을 프로퍼티에 할당
+
+    @Value("#{'hello ' + 'world'}")
+    String greeting;
+
+    @Value("#{1 eq 1}")
+    boolean trueOrFalse;
+
+    @Value("hello")
+    String hello;
+
+    //2) ${“프로퍼티"}
+    @Value("${my.value}")
+    int myValue;
+
+    //3) 표현식 안에는 프로퍼티를 사용할 수 있지만, 프로퍼티 안에서는 표현식을 사용할 수 없다
+    @Value("#{${my.value} eq 100}")
+    boolean isMyValue100;
+
+    //4) Literal Expressions : 문자열 리터럴 그대로 할당
+    @Value("#{'spring'}")
+    String spring;
+
+    //5) Inline Lists
+    @Value("#{'1,2,3,4'}")
+    List<Integer> numbers;
+
+    @Value("#{{{'x, y'}, {'a, b'}}}")
+    List listOfLists;
+
+    //6) Bean References
+    @Value("#{sample.data}")
+    int sampleData;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(conversionService);
-        System.out.println(conversionService.getClass().toString()); //(Spring Boot의 경우) WebConversionService <- DefaultFormattingConversionService를 상속하여 만듦
+        System.out.println("================");
+        System.out.println(value);
+        System.out.println(greeting);
+        System.out.println(trueOrFalse);
+        System.out.println(hello);
+        System.out.println(myValue);
+        System.out.println(isMyValue100);
+        System.out.println(spring);
+
+        System.out.println(numbers);
+        int i = 0;
+        for (Integer number : numbers) {
+            System.out.println("idx["+i+"] = " + number);
+            i++;
+        }
+
+        System.out.println(listOfLists);
+        Object first = listOfLists.get(0);
+        Object second = listOfLists.get(1);
+        System.out.println("first = " + first);
+        System.out.println("second = " + second);
+        System.out.println(sampleData);
+
+        /* SpEL의 기반 클래스 */
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("2 + 100");
+        Integer value = expression.getValue(Integer.class);
+        System.out.println(value);
     }
 }
